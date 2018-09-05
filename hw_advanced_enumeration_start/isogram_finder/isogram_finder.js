a70848e6ecfceeee1fc2460de202c6d5b8779a04
Laura Nagy <ngylaura@gmail.com>
2018-09-05 20:27:05 +0100

    passes isograms test

--- hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
+++ hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,20 @@
 const IsogramFinder = function (word) {
-
+  this.word = word;
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+  const wordCopy = this.word.split('');
+  let isogram = true;
+  const result = wordCopy.reduce( (uniques, letter) => {
+    if ( !uniques.includes(letter) ) {
+      uniques.push(letter);
+    };
+    return uniques;
+  }, []);
+  if (result.length < wordCopy.length) {
+    isogram = false;
+  };
+  return isogram;
 }
 
 module.exports = IsogramFinder;

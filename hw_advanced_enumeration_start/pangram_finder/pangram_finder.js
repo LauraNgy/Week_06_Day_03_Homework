06d73ed5ecbceb61c7f5958e37a550fbf5a2c73c
Laura Nagy <ngylaura@gmail.com>
2018-09-05 20:12:30 +0100

    passes pangram test

--- hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
+++ hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
@@ -1,9 +1,20 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.toLowerCase();
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  let pangram = false;
+  const result = [];
+  this.alphabet.forEach( (letter) => {
+    if (this.phrase.includes(letter)) {
+      result.push(letter);
+    }
+  });
+  if (result.toString() === this.alphabet.toString()) {
+    pangram = true;
+  }
+  return pangram;
 }
 
 module.exports = PangramFinder;
